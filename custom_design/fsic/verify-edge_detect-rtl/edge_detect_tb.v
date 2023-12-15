27fefc16032bf79d41c9f7117d857f90780340a4
tonyho <TonyHo@via.com.tw>
2023-12-15 22:10:22 +0800

    update code to remove below Warning messge in iverilog WARNING: edge_detect_tb.v:888: $readmemh: The behaviour for reg[...] mem[N:0]; $readmemh("...", mem); changed in the 1364-2005 standard. To avoid ambiguity, use mem[0:N] or explicit range parameters $readmemh("...", mem, start, stop);. Defaulting to 1364-2005 behavior.

--- custom_design/fsic/verify-edge_detect-rtl/edge_detect_tb.v
+++ custom_design/fsic/verify-edge_detect-rtl/edge_detect_tb.v
@@ -870,8 +870,8 @@ module edge_detect_tb #( parameter BITS=32,
 
 	reg[31:0] idx3;
   reg[31:0] frm_cnt;
-  reg[7:0]  tst_img_in_buf [TST_TOTAL_PIXEL_NUM];
-  reg[7:0]  tst_img_out_buf[TST_TOTAL_PIXEL_NUM];
+  reg[7:0]  tst_img_in_buf [0:TST_TOTAL_PIXEL_NUM];
+  reg[7:0]  tst_img_out_buf[0:TST_TOTAL_PIXEL_NUM];
   reg[31:0] tst_crc32_img_in_buf[1];
   reg[31:0] tst_crc32_img_out_buf[1];
 
